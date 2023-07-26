25988a7b2b59216b6cc7430bf73c470e96e0ad94
= <=>
2023-07-26 11:08:12 +0300

    fix: delete last dev

--- JavaApp/src/com/java/test/DbConf.java
+++
@@ -1,6 +0,0 @@
-package com.java.test;
-public class DbConf {
-    public static final String URL = "jdbc:postgresql://localhost:5432/java_app";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "pass_word";
-}
